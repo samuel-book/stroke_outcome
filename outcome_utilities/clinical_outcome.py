63089d39c17b0bcf19575c0121e06ba8f5c16d71
Michael Allen <michael.allen1966@gmail.com>
2022-08-19 08:51:14 +0100

    Add docstrings to outcome model code

--- outcome_utilities/clinical_outcome.py
+++ outcome_utilities/clinical_outcome.py
@@ -2,8 +2,75 @@ import numpy as np
 import pandas as pd
 
 class Clinical_outcome:
+    """
+    Predicts modified Rankin Scale (mRS) distributions for ischaemic stroke
+    patients depending on time to treatment with intravenous thrombolysis (IVT)
+    or mechanical thrombectomy (MT). Results are broken down for large vessel
+    occulusions (LVO) and non large vessel occlusions (nLVO).
+
+    Inputs
+    ------
+
+    A Pandas DataFrame object of mRS distributions for:
+    1) Untreated nLVO
+    2) nLVO treated at t=0 (time of stroke onset) with IVT
+    3) nLVO treated at time of no-effect (includes treatment deaths)
+    4) Untreated LVO
+    5) LVO treated at t=0 (time of stroke onset) with IVT
+    6) LVO treated with IVT at time of no-effect (includes treatment deaths)
+    7) LVO treated at t=0 (time of stroke onset) with IVT
+    8) LVO treated with IVT at time of no-effect (includes treatment deaths)
+
+    Time of IVT and MT.
+
+    Outputs
+    -------
+
+    mRS distributions, and mean mRS, for:
+    1) LVO untreated
+    2) nLVO untreated
+    3) LVO treated with IVT
+    4) LVO treated with MT
+    5) nLVO treated with IVT
+
+    mRS mean shift (compared with untreated) and Proportion of patients with
+    improved mRS for:
+    1) LVO treated with IVT
+    2) LVO treated with MT
+    3) nLVO treated with IVT
+
+    General methodology
+    -------------------
+
+    The model assumes that log odds of mRS <= x declines uniformally with time.
+    The imported distribution give mRS <= x probabilities at t=o (time of
+    stroke onset) and time of no effect. These two distributions are converted
+    to log odds and weighted according to the fraction of time, in relation to
+    when the treatment no longer has an effect, that has passed. The weighted
+    log odds distribution is converted back to probability of mRS <= x.
+
+    The time to no-effect is taken as:
+    1) 6.3 hours for IVT
+      (from Emberson et al, https://doi.org/10.1016/S0140-6736(14)60584-5.)
+    2) 8 hours for MT
+      (from Fransen et al; https://doi.org/10.1001/jamaneurol.2015.3886.
+      this analysis did not include late-presenting patients selected by
+      advanced imaging).
+
+    1,000 patients are then sampled from the untreated and treated
+    distributions (samples are taken evenly spaced across the distrubutions.
+    This gives sampled mRS distributions. The shift in mRS for each patient
+    between untreated and treated distribution is also calculated. A negative
+    shift is indicative of improvement (lower MRS disability score).
+    """
+
     def __init__(self, mrs_dists):
-        """Constructor for clinical outcome model
+        """
+        Constructor for clinical outcome model.
+
+        Input: mRS distributions for untreated, t=0 treatment, and treatment at
+        time of no effect (which also includes treatment-related excess deaths).
+
         """
         self.name = "Clinical outcome model"
 
@@ -25,6 +92,36 @@ class Clinical_outcome:
         self.mt_time_no_effect = 8 * 60
 
     def calculate_outcomes(self, time_to_ivt, time_to_mt):
+        """
+        Calls methods to model mRS populations for:
+        1) LVO untreated
+        2) nLVO untreated
+        3) LVO treated with IVT
+        4) LVO treated with MT
+        5) nLVO treated with IVT
+
+        These are converted into cumulative probabilties, mean mRS, mRS shift,
+        and proportion of patients with improved mRS.
+
+        Returns:
+        --------
+
+        A results dictionary with:
+
+        mRS distributions, and mean mRS, for:
+            1) LVO untreated
+            2) nLVO untreated
+            3) LVO treated with IVT
+            4) LVO treated with MT
+            5) nLVO treated with IVT
+
+        mRS mean shift (compared with untreated) and Proportion of patients with
+        improved mRS for:
+            1) LVO treated with IVT
+            2) LVO treated with MT
+            3) nLVO treated with IVT
+
+        """
 
         # Set up results dictionary
         results = dict()
@@ -83,6 +180,19 @@ class Clinical_outcome:
         return results
 
     def calculate_outcomes_for_lvo_ivt(self, time_to_ivt):
+        """
+        Models populations of patients (default=1000) for:
+        1) Untreated LVO
+        2) LVO treated with IVT at given time
+        3) Shift in mRS between untreated and treated
+
+        Inputs:
+        Time to IVT
+
+        Outputs:
+        A dictionary of patient population mRS as described above.
+        """
+
         # Get relevant distributions
         untreated_probs = self.mrs_distribution_probs['no_treatment_lvo']
         no_effect_logodds = self.mrs_distribution_logodds[
@@ -111,6 +221,19 @@ class Clinical_outcome:
         return results
 
     def calculate_outcomes_for_lvo_mt(self, time_to_mt):
+        """
+        Models populations of patients (default=1000) for:
+        1) Untreated LVO
+        2) LVO treated with MT at given time
+        3) Shift in mRS between untreated and treated
+
+        Inputs:
+        Time to MT
+
+        Outputs:
+        A dictionary of patient population mRS as described above.
+        """
+
         # Get relevant distributions
         untreated_probs = self.mrs_distribution_probs['no_treatment_lvo']
         no_effect_logodds = self.mrs_distribution_logodds[
@@ -139,6 +262,19 @@ class Clinical_outcome:
         return results
 
     def calculate_outcomes_for_nlvo_ivt(self, time_to_ivt):
+        """
+        Models populations of patients (default=1000) for:
+        1) Untreated nLVO
+        2) LVO treated with IVT at given time
+        3) Shift in mRS between untreated and treated
+
+        Inputs:
+        Time to IVT
+
+        Outputs:
+        A dictionary of patient population mRS as described above.
+        """
+
         # Get relevant distributions
         untreated_probs = self.mrs_distribution_probs['no_treatment_nlvo']
         no_effect_logodds = self.mrs_distribution_logodds[
